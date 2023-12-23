4a17f8998eee289cd8100251953ab5a23beefd1d
adarshahelvar <adarshaadi1997@gmail.com>
2023-12-23 10:59:35 +0530

    Storing Data of courses in Redis,to reduce initial loading

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -5,6 +5,7 @@ import cloudinary from "cloudinary";
 import { createContext } from "vm";
 import { createCourse } from "../services/course.services";
 import CourseModel from "../models/course.model";
+import { redis } from "../utils/redis";
 
 // Upload course
 export const uploadCourse = CatchAsyncError(
@@ -70,33 +71,61 @@ export const editCourse = CatchAsyncError(
 export const getSingleCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const course = await CourseModel.findById(req.params.id).select(
-        "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
-      );
-      res.status(200).json({
-        success: true,
-        message: `Course data fetched successfully`,
-         course
-      })
+      const courseId = req.params.id;
+      const isCacheExist = await redis.get(courseId);
+
+      /* 
+        What this if condition will do is if we are loading courses for first time it will skip the if condition because we dont have anything in cache data.
+        But if we are loading courses for the second time then we get all data from cache only.
+      */
+      if (isCacheExist) {
+        const course = JSON.parse(isCacheExist);
+        res.status(200).json({
+          success: true,
+          message: `Course data fetched successfully from Cache `,
+          course,
+        });
+      } else {
+        const course = await CourseModel.findById(req.params.id).select(
+          "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+        );
+        await redis.set(courseId, JSON.stringify(course));
+        res.status(200).json({
+          success: true,
+          message: `Course data fetched successfully`,
+          course,
+        });
+      }
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
     }
   }
 );
 
-// Get all courses --> without Purchasing 
+// Get all courses --> without Purchasing
 
 export const getAllCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const course = await CourseModel.find().select(
-        "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
-      );
-      res.status(200).json({
-        success: true,
-        message: `Course data fetched successfully`,
-         course
-      })
+      const isCacheExist = await redis.get("allCourses");
+      if (isCacheExist) {
+        const course = JSON.parse(isCacheExist);
+        res.status(200).json({
+          success: true,
+          message: `Course data fetched successfully from Cache `,
+          course,
+        });
+      } else {
+        const courses = await CourseModel.find().select(
+          "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+        );
+        await redis.set("allCorses", JSON.stringify(courses));
+        res.status(200).json({
+          success: true,
+          message: `Course data fetched successfully`,
+          courses,
+        });
+      }
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
     }
