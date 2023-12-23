bd8f2feb0aea3f5f4f0d4575975857a5144cea87
adarshahelvar <adarshaadi1997@gmail.com>
2023-12-23 09:49:59 +0530

    Get single course without purchasing

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -56,8 +56,27 @@ export const editCourse = CatchAsyncError(
       );
       res.status(201).json({
         success: true,
-        message: 'Course updated successfully',
-        course
+        message: "Course updated successfully",
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// Get single course-->Without purchasing
+
+export const getSingleCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const course = await CourseModel.findById(req.params.id).select(
+        "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+      );
+      res.status(200).json({
+        success: true,
+        message: `Course data fetched successfully`,
+         course
       })
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authorizationRoles, isAuthenticated } from "../middleware/auth";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import { editCourse, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 const courseRouter = express.Router();
 
 courseRouter.post('/createCourse',isAuthenticated, authorizationRoles("admin"), uploadCourse);
 courseRouter.put('/edit-course/:id',isAuthenticated, authorizationRoles("admin"), editCourse);
+courseRouter.get('/get-course/:id', getSingleCourse);
 
 export default courseRouter;
