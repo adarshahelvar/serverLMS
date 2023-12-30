ebe23db4e3f314022ec50d37bc3d17e93dda3cb4
adarshahelvar <adarshaadi1997@gmail.com>
2023-12-30 16:55:36 +0530

    Update notification Unread to read

--- routes/notification.route.ts
+++ routes/notification.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getNotification } from "../controllers/notification.controller";
+import { getNotification, updateNotification } from "../controllers/notification.controller";
 import { authorizationRoles, isAuthenticated } from "../middleware/auth";
 
 const notificationRouter = express.Router();
 
 notificationRouter.get('/get-all-notification',isAuthenticated , authorizationRoles("admin") , getNotification);
+notificationRouter.put('/update-notification/:id',isAuthenticated , authorizationRoles("admin") , updateNotification);
 
 
 export default notificationRouter;
