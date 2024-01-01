368d4d6d842697be0b39081d374ccda361ea84dd
adarshahelvar <adarshaadi1997@gmail.com>
2024-01-01 16:46:46 +0530

    Get all layout data

--- routes/layout.route.ts
+++ routes/layout.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { authorizationRoles, isAuthenticated } from "../middleware/auth";
-import { createLayout, editLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller";
 
 const layoutRouter = express.Router();
 
 layoutRouter.post('/create-layout',isAuthenticated , authorizationRoles("admin") , createLayout);
 layoutRouter.put('/edit-layout',isAuthenticated , authorizationRoles("admin") , editLayout);
+layoutRouter.get('/get-layout' , getLayoutByType);
 
 
 export default layoutRouter;
