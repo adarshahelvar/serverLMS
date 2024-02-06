e2cee05c9b2a92425f0b39c00c4a625f86d08369
adarshahelvar <adarshaadi1997@gmail.com>
2024-02-06 12:24:48 +0530

    cors origin update

--- app.ts
+++ app.ts
@@ -30,7 +30,7 @@ app.use(cookieParser());
 // );
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://elearningadarshahelvar.vercel.app'],
     credentials: true,
   })
 );
